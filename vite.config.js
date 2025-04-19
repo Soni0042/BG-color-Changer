061d30d35337e0df362a14a5e85210613a0a5d01
Soni0042 <piyushsverma1806@gmail.com>
2025-04-20 01:05:31 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"BG-color-changer",
+  base:"/BG-color-Changer",
   plugins: [react(), tailwindcss(),],
   
 })
